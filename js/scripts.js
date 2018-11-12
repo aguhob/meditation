0746cba2d427370dce50b2c872d27e6c62be1106
Minda Aguhob <minan@Mindas-MacBook-Pro.local>
2018-11-12 15:21:53 -0800

    update css and index

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$( document ).ready( function () {
+
+    $("#chakras dt").click( function (e) {
+
+        console.log($(this).next().html() );
+        
+        if ( $(this).next().css("display") == "none") { 
+            $(this).next().fadeIn(2000).show();
+        } else {
+            $(this).next().css("display", "none");
+        }
+        
+    });
+});
+
+
+// this code makes all of the items fade in one at a time
---
+++ notes.js
@@ -0,0 +1,29 @@
+//NOTES
+//
+//$( document ).ready( function () {
+//
+//    $("#chakras dt").click( function (e) {
+//
+//        console.log($(this).next().html() );
+//
+//        //if no 2nd arguments, will grab it
+//        if ( $(this).next().css("display") == "none") 
+//        
+//        {
+//            //^change from chakras dd
+//            $(this).next().fadeIn(2000).show();
+///* replaced .css("display") Eduardo recommends instead of using "show" */
+//            
+//        } else {
+//
+//            $("#chakras dd").css("display", "none");
+//
+//        }
+//
+//
+//
+//    });
+//
+//});
+//
+//
